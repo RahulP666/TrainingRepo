36cf67aeecdca7c9af067d4c098fbcc3d3471bb8
Rahul Pagare <rahul.b.pagare@duckcreek.com>
2023-10-20 08:19:04 +0530

    Changed Artifactory Version

---
+++ AzureTrainingClassLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AzureTrainingClassLibrary
+{
+    public class AzClass
+    {
+
+        public string GetName(string Fname, string Lname)
+        {
+            string FullName;
+            FullName = Fname + " " + Lname;
+            return FullName;
+        }
+    }
+}
